77d8104c3800afaec8aeb6fc091d20aece148bdf
anndrem <dealmeida.andre08@gmail.com>
2025-09-30 00:49:46 -0300

    feat: sistemas de filas bullmq nas notificações

---
+++ src/workers/notificationWorker.ts
@@ -0,0 +1,23 @@
+import { Worker } from "bullmq"
+import IORedis from "ioredis"
+import NotificationsRepository from "../repositories/Notifications"
+import { handleMessage } from "../Handles/NotificatiosHandler"
+
+
+const connection = new IORedis(process.env.IOREDIS_URL ?? "", { maxRetriesPerRequest: null })
+const handleEvent = new handleMessage(new NotificationsRepository())
+
+const notificationsWorker = new Worker(
+    "notificationsQueue",
+    async job => {
+        try {
+            const {id_destinatario, id_actor, id_post, type} = job.data
+            console.log(job.data)
+            return (await handleEvent.createNotification(id_destinatario, id_actor, id_post, type))
+        } catch (err) {
+            console.error("erro ao inserir notificação", err)
+        }
+    }
+, { connection })
+
+export default notificationsWorker;
